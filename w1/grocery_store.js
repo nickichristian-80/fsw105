fd59bcb7fd127af2c461340559557cb3d2c05617
nickichristian-80 <nicki.christian@bryanuniversity.edu>
2020-07-28 17:31:50 -0500

    commit

--- w1/grocery_store.js
+++ w1/grocery_store.js
@@ -2,9 +2,9 @@ var shopper = {
     name: "Nicki",
     age: 40,
     isAlive: true,
-    groceryCart: ["milk, cheese, bread"],
-    fullInfo = function(){ 
-        console.log("name" + " " + "groceryCart")        
-    }
-    
+    groceryCart: ["milk", "cheese", "bread"],
+    fullInfo: function() { 
+        return this.name + " " + this.groceryCart;                
+    }     
 }
+console.log(shopper.fullInfo())
